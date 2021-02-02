34e9259adce5ecfc890287220e284234a6838f7f
blackdog <blackdogcode@gmail.com>
2021-02-02 18:03:21 +0900

    Flask SQLAlchemy DB Connection and Create DB

--- FlaskProject/app.py
+++ FlaskProject/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+uri = 'postgresql+psycopg2://blackdog:#mu1561511@localhost:5432/sandbox'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
