f9e4ac93081337e155f7c995b3a9a0ab6948380f
overload <blackdogcode@gmail.com>
2021-02-10 00:45:00 +0900

    LEFT JOIN refatctoring

--- Postgres/joining_tables/left_join.sql
+++ Postgres/joining_tables/left_join.sql
@@ -19,8 +19,9 @@ LEFT JOIN inventory
 
 
 SELECT
-    film.film_id,
-    inventory.film_id
+    film.film_id            AS film_id,
+    film.title              AS film_title,
+    inventory.inventory_id  AS inventory_id
 FROM
     film
 LEFT JOIN inventory
