ce1160c7364a94c6a0e726aefbff5cf529709628
overload <blackdogcode@gmail.com>
2021-02-14 23:44:19 +0900

    Modify Sample Records

--- Postgres/set_operation/union.sql
+++ Postgres/set_operation/union.sql
@@ -3,53 +3,64 @@
 -- Create Sample Tables
 CREATE TABLE amazon
 (
-    category_name VARCHAR NOT NULL
+    category_name VARCHAR NOT NULL,
+    product_name  VARCHAR NOT NULL
 );
 
 CREATE TABLE alibaba
 (
-    category_name VARCHAR NOT NULL
+    category_name VARCHAR NOT NULL,
+    product_name  VARCHAR NOT NULL
 );
 
 -- Insert Sample Records
-INSERT INTO amazon (category_name)
+INSERT INTO amazon (category_name, product_name)
 VALUES
-    ('automotive'),
-    ('computer'),
-    ('game'),
-    ('book'),
-    ('fashion');
+    ('automotive', 'Tesla Model S'),
+    ('computer', 'Macbook M1 Pro'),
+    ('game', 'Xbox Series X'),
+    ('book', 'Problem Solving'),
+    ('security', 'Ucam');
 
-INSERT INTO alibaba (category_name)
+INSERT INTO alibaba (category_name, product_name)
 VALUES
-    ('computer'),
-    ('fashion'),
-    ('automotive'),
-    ('music'),
-    ('pet');
+    ('computer', 'Macbook M1 Pro'),
+    ('security', 'Ucam'),
+    ('automotive', 'Tesla Model S'),
+    ('software', 'Matlab'),
+    ('game', 'Playstation 5');
 
 
 -- UNION: Only Unique Rows
 SELECT
-    amazon.category_name AS category_name
+    amazon.category_name AS category_name,
+    amazon.product_name  AS product_name
 FROM
     amazon
 UNION
 SELECT
-    alibaba.category_name AS category_name
+    alibaba.category_name AS category_name,
+    alibaba.product_name  AS product_name
 FROM
-    alibaba;
+    alibaba
+ORDER BY
+    category_name, product_name;
 
--- UNION ALL: Allow Duplicated Rows
+
+-- UNION ALL: Allow Duplicated Records
 SELECT
-    amazon.category_name AS category_name
+    amazon.category_name AS category_name,
+    amazon.product_name  AS product_name
 FROM
     amazon
 UNION ALL
 SELECT
-    alibaba.category_name AS category_name
+    alibaba.category_name AS category_name,
+    alibaba.product_name  AS product_name
 FROM
-    alibaba;
+    alibaba
+ORDER BY
+    category_name, product_name;
 
 -- Drop Tables
 DROP TABLE amazon;
