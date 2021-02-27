ffd8a48b339c816fa7055f2feaf6672742ea2870
overload <blackdogcode@gmail.com>
2021-02-27 16:21:19 +0900

    create tesla car model

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://blackdog:blackdog@localhost:5432/sandbox'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/tesla'
+
 db = SQLAlchemy(app)
 
 
-class Person(db.Model):
-    __tablename__ = 'persons'
+class CarModel(db.Model):
+    __tablename__ = 'car_models'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
@@ -18,8 +18,7 @@ db.create_all()
 
 @app.route('/')
 def index():
-    person = Person.query.first()
-    return f'Hello {person.name}!'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
