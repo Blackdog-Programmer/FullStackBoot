ffc41e28447c8cf651da77e9b8a62207010ba0de
overload <blackdogcode@gmail.com>
2021-03-02 18:23:21 +0900

    add __repr__ method for tesla car model class

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -8,10 +8,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class CarModel(db.Model):
-    __tablename__ = 'car_models'
+class Model(db.Model):
+    __tablename__ = 'models'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'Tesla Model Name: {self.name}'
 
 
 db.create_all()
@@ -19,8 +22,12 @@ db.create_all()
 
 @app.route('/')
 def index():
-    tesla_model = CarModel.query.first()
-    return tesla_model.name
+    tesla_models = Model.query.all()
+    if len(tesla_models) != 0:
+        for model in tesla_models:
+            return repr(model)
+    else:
+        return 'There are not tesla models launched yet'
 
 
 if __name__ == '__main__':
