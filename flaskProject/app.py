ec394bfcf775da9871537b6979ada11edb8e7e30
overload <blackdogcode@gmail.com>
2021-03-08 19:10:06 +0900

    set color table of tank model and relationship

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -2,19 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tanks'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tank'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
 class Model(db.Model):
-    __tablename__ = 'models'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=False)
 
+    colors = db.relationship('Color', backref='model', lazy=True)
+
     def __repr__(self):
         return f'Tank Model: {self.name}'
+
+
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    model_id = db.Column(db.Integer, db.ForeignKey('model.id'), nullable=False)
 
 
 db.create_all()
