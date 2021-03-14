1d0d13c7ab966ba378e66817c89b936914bd9ba5
overload <blackdogcode@gmail.com>
2021-03-14 13:59:37 +0900

    add product class(tables)

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import joinedload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/eshop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    category = db.relationship('Category', backref=db.backref('products', lazy=True))
+
+    def __repr__(self):
+        return f'{self.name}'
 
 
 class Category(db.Model):
@@ -21,8 +32,12 @@ db.create_all()
 
 @app.route('/')
 def index():
-    category = Category.query.first()
-    return f'{category.name}'
+    ret = ''
+    query = Category.query.options(joinedload('products'))
+    for category in query:
+        ret += f'{str(category)} {str(category.products)}\n'
+
+    return ret
 
 
 if __name__ == '__main__':
