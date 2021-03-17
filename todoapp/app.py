cfe7a50783e24412882b7dcb435aac56fb7e85c3
overload <blackdogcode@gmail.com>
2021-03-17 15:23:36 +0900

    read todo list from todos db table

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<ToDo: {self.id}, {self.description}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    },  {
-        'description': 'Todo 2'
-    },  {
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=ToDo.query.all())
 
 
 if __name__ == '__main__':
