e0ca802eb5a92f19fe1637e957f4d39657c36965
blackdogcode <blackdogcode@gmail.com>
2021-03-23 15:02:17 +0900

    Convert to AJAX data controlling

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -23,12 +23,14 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    desc = request.form.get('description', '')
+    desc = request.get_json()['description']
     todo = ToDo(description=desc)
     db.session.add(todo)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
