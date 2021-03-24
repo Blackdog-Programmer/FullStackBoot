1fdbbc020fa07641f17d861bc2789c65f9e542e2
blackdogcode <blackdogcode@gmail.com>
2021-03-24 13:09:47 +0900

    add 'completed' column on ToDo class

--- todoapp/app.py
+++ todoapp/app.py
@@ -17,6 +17,7 @@ class ToDo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<ToDo: {self.id}, {self.description}>'
